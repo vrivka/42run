4e2b764e1663e453bf5f1c1972df518de5d22ac3
vrivka <vladkolezov@gmail.com>
2022-08-28 12:23:12 +0300

    Add textures for clusters

--- src/main/resources/fragment.glsl
+++ src/main/resources/fragment.glsl
@@ -21,6 +21,22 @@ void main()
             fragColor = vec4(color - 0.3, 1);
         }
     } else {
-        fragColor = texture(texture_sampler, TextCords);
+        if (Normal == vec3(0, 0, 0)) {
+            fragColor = texture(texture_sampler, TextCords);
+        } else {
+            vec3 an = abs(Normal);
+            vec4 textRGBA = texture(texture_sampler, TextCords);
+            vec3 textRGB = textRGBA.rgb;
+            float alpha = textRGBA.a;
+
+            if (an.x >= 0.5 || (an.y < 0.5 && an.z < 0.5)) {
+                textRGBA = vec4(textRGB - 0.1, alpha);
+            } else if (an.y >= 0.5 || (an.x < 0.5 && an.z < 0.5)) {
+                textRGBA = vec4(textRGB - 0.2, alpha);
+            } else if (an.z >= 0.5 || (an.x < 0.5 && an.y < 0.5)) {
+                textRGBA = vec4(textRGB - 0.3, alpha);
+            }
+            fragColor = textRGBA;
+        }
     }
 }
